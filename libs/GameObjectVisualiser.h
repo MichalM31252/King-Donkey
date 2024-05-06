946e290fb20749244f2d2a379e57000d98f647f4
Michal Malinowski <65298857+TheRiceDealer@users.noreply.github.com>
2024-05-06 23:50:35 +0200

    Feat created new class GameObjectVisualiser

---
+++ libs/GameObjectVisualiser.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "./game_screen/ScreenManager.h"
+
+class GameObjectVisualiser {
+private:
+	ScreenManager* screenManager;
+
+
+};
--- libs/StageManager.h
+++ libs/StageManager.h
@@ -3,11 +3,11 @@
 #include "./game_events/CollisionManager.h"
 #include "./PhysicsManager.h"
 #include "./GameObjectFactory.h"
+#include "./GameObjectVisualiser.h"
 
 class StageManager {
 private:
 	CollisionManager* collisionManager;
 	PhysicsManager* physicsManager;
 	GameObjectFactory* gameObjectFactory;
-
 };
