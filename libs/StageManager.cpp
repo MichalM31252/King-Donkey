93e4b642e333fd14632cdf8193111bea8a8b727f
Michal Malinowski <65298857+TheRiceDealer@users.noreply.github.com>
2024-05-25 21:39:45 +0200

    Feat added a new class StageManager where all of the managers will be stored

--- libs/StageManager.cpp
+++ libs/StageManager.cpp
@@ -1,1 +1,23 @@
 #pragma once
+
+#include "StageManager.h"
+
+StageManager::StageManager()
+{
+
+}
+
+StageManager::StageManager(GameObjectContainer& gameObjectContainer)
+{
+	// assign gameObjectContainer to each one of these managers
+	this->collisionManager = new CollisionManager();
+	this->screenManager = new ScreenManager();
+	this->collisionManager = new CollisionManager();
+	this->gameObjectFactory = new GameObjectFactory();
+	this->keyboardManager = new KeyboardManager();
+}
+
+StageManager::~StageManager()
+{
+
+}
