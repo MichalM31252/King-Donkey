8676ab30f3434b00be81c5de5f6619f0d13c581b
Michal Malinowski <65298857+TheRiceDealer@users.noreply.github.com>
2024-09-15 12:11:33 +0200

    Ref classes "Game" and "StageManager" now only use smart pointers for automatic memory management

--- libs/StageManager.h
+++ libs/StageManager.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include "CollisionResolver.h"
 #include "PhysicsManager.h"
 #include "GameObjectFactory.h"
@@ -8,11 +9,11 @@
 #include "GameObjectContainer.h"
 
 class StageManager {
-private:
-    CollisionResolver* collisionResolver;
-    GameObjectFactory* gameObjectFactory;
-    KeyboardManager* keyboardManager;
-    ScreenManager* screenManager;
+public:
+    std::unique_ptr<CollisionResolver> collisionResolver;
+    std::unique_ptr<GameObjectFactory> gameObjectFactory;
+    std::unique_ptr<KeyboardManager> keyboardManager;
+    std::unique_ptr<ScreenManager> screenManager;
 
     StageManager() = default;
     // StageManager(GameObjectContainer* gameObjectContainer);
