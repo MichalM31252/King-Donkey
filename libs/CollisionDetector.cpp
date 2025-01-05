79bbe8ab650105668aa9ccb9b9a79ef39f97d9a3
Michal Malinowski <65298857+TheRiceDealer@users.noreply.github.com>
2025-01-05 04:18:15 +0100

    Feat added correctly rotating the platform relative to the center of the platform

--- libs/CollisionDetector.cpp
+++ libs/CollisionDetector.cpp
@@ -8,194 +8,118 @@ bool CollisionDetector::isCollisionBetweenRects(const SDL_Rect a, const SDL_Rect
 // Platform
 //////////////////////////////////////////////////////////////////////////
 
-void printPlatformVerticesBeforeRotation(const std::shared_ptr<const Platform>& platform) {
-    // Extract platform properties
-    int platformX = platform->rect.x;
-    int platformY = platform->rect.y;
-    int platformWidth = platform->rect.w;
-    int platformHeight = platform->rect.h;
+struct Point {
+    double x, y;
+};
 
-    // Compute the coordinates of the four vertices before rotation
-    double left = platformX;
-    double right = platformX + platformWidth;
-    double top = platformY;
-    double bottom = platformY + platformHeight;
+Point calculateCenter(const Point& v1, const Point& v2, const Point& v3, const Point& v4) {
+    double centerX = (v1.x + v2.x + v3.x + v4.x) / 4.0;
+    double centerY = (v1.y + v2.y + v3.y + v4.y) / 4.0;
+    return { centerX, centerY };
+}
+
+Point rotatePoint(const Point& point, const Point& center, double angleDegrees) {
+    double angleRadians = angleDegrees * M_PI / 180.0; // Convert degrees to radians
+    double cosTheta = cos(angleRadians);
+    double sinTheta = sin(angleRadians);
+
+    // Translate point to origin
+    double translatedX = point.x - center.x;
+    double translatedY = point.y - center.y;
+
+    // Rotate the point
+    double rotatedX = translatedX * cosTheta - translatedY * sinTheta;
+    double rotatedY = translatedX * sinTheta + translatedY * cosTheta;
+
+    // Translate back to center
+    return { rotatedX + center.x, rotatedY + center.y };
+}
+
+void rotateRectangle(Point vertices[], double angleDegrees) {
+    // Calculate the center before rotation
+    Point center = calculateCenter(vertices[0], vertices[1], vertices[2], vertices[3]);
+
+    // Print vertices and center before rotation
+    std::cout << "Center of the rectangle before rotation: ";
+    std::cout << "(" << std::round(center.x) << ", " << std::round(center.y) << ")" << std::endl;
 
     std::cout << "Vertices before rotation:" << std::endl;
-    std::cout << "(" << left << ", " << top << ")" << std::endl;       // Top-left
-    std::cout << "(" << right << ", " << top << ")" << std::endl;      // Top-right
-    std::cout << "(" << right << ", " << bottom << ")" << std::endl;   // Bottom-right
-    std::cout << "(" << left << ", " << bottom << ")" << std::endl;    // Bottom-left
+    for (int i = 0; i < 4; ++i) {
+        std::cout << "(" << std::round(vertices[i].x) << ", " << std::round(vertices[i].y) << ")" << std::endl;
+    }
+
+    // Rotate each vertex
+    for (int i = 0; i < 4; ++i) {
+        vertices[i] = rotatePoint(vertices[i], center, angleDegrees);
+    }
+
+    // Calculate the center after rotation (it should remain the same)
+    Point centerAfterRotation = calculateCenter(vertices[0], vertices[1], vertices[2], vertices[3]);
+
+    // Print vertices and center after rotation
+    std::cout << "\nCenter of the rectangle after rotation: ";
+    std::cout << "(" << std::round(centerAfterRotation.x) << ", " << std::round(centerAfterRotation.y) << ")" << std::endl;
+
+    std::cout << "Vertices after rotation by " << angleDegrees << " degrees:" << std::endl;
+    for (int i = 0; i < 4; ++i) {
+        std::cout << "(" << std::round(vertices[i].x) << ", " << std::round(vertices[i].y) << ")" << std::endl;
+    }
 }
 
-void printPlatformVerticesAfterRotation(const std::shared_ptr<const Platform>& platform) {
-    // Extract platform properties
-    int platformX = platform->rect.x;
-    int platformY = platform->rect.y;
-    int platformWidth = platform->rect.w;
-    int platformHeight = platform->rect.h;
-    double platformAngle = platform->angle; // angle is in radians and is clockwise
-
-    // Compute the center of the platform
-    double centerX = platformX + platformWidth / 2.0;
-    double centerY = platformY + platformHeight / 2.0;
-
-    // Compute the coordinates of the four vertices before rotation
-    double left = -platformWidth / 2.0;
-    double right = platformWidth / 2.0;
-    double top = -platformHeight / 2.0;
-    double bottom = platformHeight / 2.0;
-
-    // Rotate each vertex around the center
-    double cosTheta = cos(platformAngle);
-    double sinTheta = sin(platformAngle);
-
-    // Top-left
-    double topLeftX = centerX + (left * cosTheta - top * sinTheta);
-    double topLeftY = centerY + (left * sinTheta + top * cosTheta);
-
-    // Top-right
-    double topRightX = centerX + (right * cosTheta - top * sinTheta);
-    double topRightY = centerY + (right * sinTheta + top * cosTheta);
-
-    // Bottom-right
-    double bottomRightX = centerX + (right * cosTheta - bottom * sinTheta);
-    double bottomRightY = centerY + (right * sinTheta + bottom * cosTheta);
-
-    // Bottom-left
-    double bottomLeftX = centerX + (left * cosTheta - bottom * sinTheta);
-    double bottomLeftY = centerY + (left * sinTheta + bottom * cosTheta);
-
-    // Print the rotated coordinates
-    std::cout << "Vertices after rotation:" << std::endl;
-    std::cout << "(" << topLeftX << ", " << topLeftY << ")" << std::endl;
-    std::cout << "(" << topRightX << ", " << topRightY << ")" << std::endl;
-    std::cout << "(" << bottomRightX << ", " << bottomRightY << ")" << std::endl;
-    std::cout << "(" << bottomLeftX << ", " << bottomLeftY << ")" << std::endl;
+// Helper function to compute the cross product of vectors AB and AP
+double crossProduct(const Point& A, const Point& B, const Point& P) {
+    return (B.x - A.x) * (P.y - A.y) - (B.y - A.y) * (P.x - A.x);
 }
 
-//bool CollisionDetector::isGameObjectOnTopOfAnyPlatform(const std::shared_ptr<const GameObject>& gameObject, const std::shared_ptr<const PlatformContainer>& platformHolder) {
-//	// TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING
-//	// TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING
-//	// TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING
-//
-//	auto normalPlatform = platformHolder->platforms[0]; // 1, 400, 400, 8, 0
-//	if (isPointInsidePlatform(100, 404, normalPlatform)) { // should be true
-//		std::cout << "true" << std::endl;
-//	}
-//	else {
-//		std::cout << "false" << std::endl;
-//	}
-//
-//	if (isPointInsidePlatform(500, 404, normalPlatform)) { // should be false
-//		std::cout << "true" << std::endl;
-//	}
-//	else {
-//		std::cout << "false" << std::endl;
-//	}
-//
-//	if (isPointInsidePlatform(200, 390, normalPlatform)) { // should be false
-//		std::cout << "true" << std::endl;
-//	}
-//	else {
-//		std::cout << "false" << std::endl;
-//	}
-//
-//	if (isPointInsidePlatform(201, 404, normalPlatform)) { // should be true
-//		std::cout << "true" << std::endl;
-//	}
-//	else {
-//		std::cout << "false" << std::endl;
-//	}
-//
-//
-//	auto tiltedPlatform = platformHolder->platforms[1]; // 380, 350, 141, 8, 135
-//	if (isPointInsidePlatform(440, 360, tiltedPlatform)) { // should be false
-//        std::cout << "true" << std::endl;
-//	}
-//    else {
-//        std::cout << "false" << std::endl;
-//    }
-//
-//	if (isPointInsidePlatform(420, 370, tiltedPlatform)) {// should be false
-//		std::cout << "true" << std::endl;
-//	}
-//	else {
-//		std::cout << "false" << std::endl;
-//	}
-//
-//	if (isPointInsidePlatform(450, 354, tiltedPlatform)) { // should be true
-//		std::cout << "true" << std::endl;
-//	}
-//    else {
-//        std::cout << "false" << std::endl;
-//    }
-//    
-//    
-//    return false;
-//}
+// Function to check if a point is inside the rectangle
+bool isPointInsideRectangle(Point vertices[], const Point& point) {
+    // Check the cross product for each edge
+    for (int i = 0; i < 4; ++i) {
+        Point A = vertices[i];
+        Point B = vertices[(i + 1) % 4]; // Next vertex (loop back to 0 after the last vertex)
+        if (crossProduct(A, B, point) < 0) {
+            return false; // Point is outside for this edge
+        }
+    }
+    return true; // Point is inside for all edges
+}
+
+bool CollisionDetector::isPointInsidePlatform(double x, double y, std::shared_ptr<Platform>& platform) {
+	// Get the vertices of the platform
+	Point vertices[4] = {
+		{ platform->rect.x, platform->rect.y },
+		{ platform->rect.x + platform->rect.w, platform->rect.y },
+		{ platform->rect.x + platform->rect.w, platform->rect.y + platform->rect.h },
+		{ platform->rect.x, platform->rect.y + platform->rect.h }
+	};
+
+	// Rotate the rectangle if the platform is tilted
+	if (platform->angle != 0) {
+		rotateRectangle(vertices, platform->angle);
+	}
+
+	// Check if the point is inside the rectangle
+	return isPointInsideRectangle(vertices, { x, y });
+}
 
 bool CollisionDetector::isGameObjectInsideAnyPlatform(const std::shared_ptr<const GameObject>& gameObject, const std::shared_ptr<const PlatformContainer>& platformHolder) {
-	for (int i = 0; i < platformHolder->getNumberOfElements(); i++) {
-		if (isGameObjectInsidePlatform(gameObject, platformHolder->platforms[i])) {
-			return true;
-		}
-	}
-	return false;
+    for (int i = 0; i < platformHolder->getNumberOfElements(); i++) {
+        if (isGameObjectInsidePlatform(gameObject, platformHolder->platforms[i])) {
+            return true;
+        }
+    }
+    return false;
 }
 
-bool CollisionDetector::isGameObjectInsidePlatform(const std::shared_ptr<const GameObject>& gameObject, std::shared_ptr<Platform> platform) {
-	// First thing I thought about is to check if the rectangle of the gameObject is inside the rectangle of the platform
-    // but this is not enough, when I'll implement a tilted platform it will not work
-	// a platform has a starting x position, starting y position, height, width and a slope
-	// there is no way to implement here the slope when using isCollisionBetweenRects method 
-	// so I need to implement a new method that will check if the gameObject is inside the platform
-
-    // The second thing is that, now a platform has height and width other than 1 pixel
-	// which means that the player won't be able to pass throught it by accident like before
-	// so I need to implement a mechanism that will check each point of the player's rectangle
-	// THEN take in consideration the platforms height and width and slope and check if the player is inside the platform
-
-	for (int i = 0; i < 4; i++) {
+bool CollisionDetector::isGameObjectInsidePlatform(const std::shared_ptr<const GameObject>&gameObject, std::shared_ptr<Platform> platform) {
+    for (int i = 0; i < 4; i++) {
 		int x = gameObject->xpos + (i % 2) * gameObject->destRect.w;
 		int y = gameObject->ypos + (i / 2) * gameObject->destRect.h;
-		if (isPointInsidePlatform(x, y, platform)) {
-            printPlatformVerticesBeforeRotation(platform);
-            printPlatformVerticesAfterRotation(platform);
+        if (isPointInsidePlatform(x, y, platform)) {
             return true;
-		}
-	}
-
-	return false;
-}
-
-bool CollisionDetector::isPointInsidePlatform(int x, int y, std::shared_ptr<Platform>& platform) {
-	// The values of x grow to the right and the values of y grow to the bottom
-	int platformX = platform->rect.x;
-	int platformY = platform->rect.y;
-	int platformWidth = platform->rect.w;
-	int platformHeight = platform->rect.h;
-	double platformAngle = platform->angle; // the angle is always clockwise
-
-    // Compute the center of the platform
-    double centerX = platformX + platformWidth / 2.0;
-    double centerY = platformY + platformHeight / 2.0;
-
-    // Translate point to the platform's coordinate system (centered at the rectangle's center)
-    double translatedX = x - centerX;
-    double translatedY = y - centerY;
-
-    // Rotate the point counterclockwise by the platform's angle to "unrotate" it
-    double cosTheta = cos(-platformAngle);
-    double sinTheta = sin(-platformAngle);
-    double unrotatedX = translatedX * cosTheta - translatedY * sinTheta;
-    double unrotatedY = translatedX * sinTheta + translatedY * cosTheta;
-
-    // Check if the unrotated point lies within the bounds of the axis-aligned rectangle
-    double halfWidth = platformWidth / 2.0;
-    double halfHeight = platformHeight / 2.0;
-
-    return (unrotatedX >= -halfWidth && unrotatedX <= halfWidth && unrotatedY >= -halfHeight && unrotatedY <= halfHeight);
+        }
+    }
+    return false;
 }
 
 // Ladder
